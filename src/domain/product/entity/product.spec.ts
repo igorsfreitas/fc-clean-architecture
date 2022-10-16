0a6ba691821d4a9ead0c99279d77a3b593a3be06
Igor Freitas <igor_s_freitas@hotmail.com>
2022-10-16 08:27:26 -0300

    feat: Product notification and tests

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -3,14 +3,26 @@ import Product from "./product";
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+      let product = new Product("", "John", 100);
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+      let product = new Product("123", "", 100);
+    }).toThrowError("product: Name is required");
+  });
+
+  it("should throw error when price is empty", () => {
+    expect(() => {
+      let product = new Product("123", "", -1);
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when name and id and price are empty", () => {
+    expect(() => {
+      let customer = new Product("", "", -1);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
   });
 
   it("should throw error when price is less than zero", () => {
